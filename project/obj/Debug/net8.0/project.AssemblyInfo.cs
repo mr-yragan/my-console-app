dab164331446ca15844ce1fa20b10a5d887d4e09
mr-yragan <kondratyev120307@gmail.com>
2025-03-01 21:55:14 +0300

    Update project

--- project/obj/Debug/net8.0/project.AssemblyInfo.cs
+++ project/obj/Debug/net8.0/project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+677636ac42716c9952aa8064605f2ec945babe4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
